a870cdb672b3e97642e209c0dc9c5d880796467e
nastusya <nastasuper5@gmail.com>
2016-11-20 21:06:56 +0200

    Create MyFragment2.java

---
+++ MyFragment2.java
@@ -0,0 +1,22 @@
+package androidhive.recyclerview;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+
+public class MyFragment2 extends android.support.v4.app.Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment2_main, container, false);
+        return view;
+    }
+    }
